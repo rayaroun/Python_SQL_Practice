c059550e6589f0139869f4fe0173c2e72f3c115a
Aroun <ray.aroun@gmail..com>
2022-08-16 21:22:59 -0700

    more problems no jobs

---
+++ Python/Find Leaves of Binary Tree.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+from typing import Optional, List
+
+import collections
+
+class Solution:
+    def findLeaves(self, root):
+        def helper(root):
+            if not root: return 0
+            left = helper(root.left)
+            right = helper(root.right)
+            level = max(left, right) + 1
+            dic[level] += root.val,
+            return level
+        dic = collections.defaultdict(list)
+        helper(root)
+        return list(dic.values())
+
+
+
+root = TreeNode(6)
+root.left = TreeNode(2)
+root.right = TreeNode(6)
+root.right.left = TreeNode(10)
+root.right.right = TreeNode(2)
+
+ob = Solution()
+
+ob.findLeaves(root)
+
+        
--- Python/temp.py
+++ Python/temp.py
@@ -1,9 +1,54 @@
+# class CategoryTree:
+
+#     def __init__(self):
+
+#         self.cate = {}
+
+#         pass
+
+#     def add_category(self, category, parent):
+
+#         # if (category in self.cate.values()) or (category in self.cate.keys()) :
+#         #     raise KeyError("Value cannot be added once it has been added")
+#         # elif parent and (parent not in self.cate.keys()):
+#         #     raise KeyError("Value cannot be added once it has been added")
 
 
 
-a = [[1,2,3,4,56]]
+#         if (category) and (not parent):
+#             self.cate[category] = []
+            
+#         # elif parent not in self.cate.keys():
+#         #     self.cate[parent] = []
+#         #     self.cate[parent].append(category)
 
-dict1 = { 'a' : 1, 'b' : 2}
+#         else:
+#             self.cate[parent].append(category)
 
-for key, values in dict1.items():
-    print (key, values)
+#         pass
+
+#     def get_children(self, parent):
+#         return self.cate[parent]
+
+# if __name__ == "__main__":
+#     c = CategoryTree()
+#     c.add_category('A', None)
+#     c.add_category('B', 'A')
+#     c.add_category('C', 'A')
+#     c.add_category('X','J')
+
+#     print(','.join(c.get_children('A') or []))
+
+
+
+import collections
+
+dic = collections.defaultdict(list)
+
+
+dic[5] += 6
+
+print(dic)
+
+
+
